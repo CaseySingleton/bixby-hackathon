8f6c5bea7900ce5ba50f2caf59234a5677189e8e
Dylan Sotomayor <dysotoma@e1z4r6p21.42.us.org>
2019-04-12 16:51:15 -0700

    working auth and search

--- bix_mix_dj.test/code/Pause.js
+++ bix_mix_dj.test/code/Pause.js
@@ -2,7 +2,7 @@
  var config = require('config')
  var http = require('http')
 module.exports.function = function auth () {
-  http.oauthPutUrl("https://api.spotify.com/v1/me/player/pause")
-  
+  http.oauthGetUrl("https://api.spotify.com/v1/search?q=Muse&type=track")
+  // http.oauthGetUrl(url, options)
   return {}
 }
