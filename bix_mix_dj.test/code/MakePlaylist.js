f5ca632b4b99af3dba2ea41493655f37c18b943d
Dylan Sotomayor <dysotoma@e1z4r6p21.42.us.org>
2019-04-16 19:48:56 -0700

    update with near search and genre .js

--- bix_mix_dj.test/code/MakePlaylist.js
+++ bix_mix_dj.test/code/MakePlaylist.js
@@ -1,18 +1,25 @@
  var http = require('http')
  var console = require('console')
 module.exports.function = function makePlaylist (Tracks, playlistname) {
-
-  // function strncmp(str1, str2, n) {
-  // str1 = str1.substring(0, n);
-  // str2 = str2.substring(0, n);
-  // return ( ( str1 == str2 ) ? 0 : (( str1 > str2 ) ? 1 : -1 ))
-// }
-  console.log(Tracks)
-  let uris
-  for (track in Tracks)
-    uris += uris + "spotify:track" + track.id
+  var response = http.oauthPostUrl("https://api.spotify.com/v1/me/playlists", JSON.stringify({"name": playlistname, "description": "a test to remove or keep you decide", "public": true}))
+// playlistname = "Indie Chillout"
+  console.log(playlistname)
+  let uris = Tracks.tracks.map((track) => {return ("spotify:track:" + track.id)}).toString()
   console.log(uris)
-    // var response = http.oauthPostUrl("https://api.spotify.com/v1/playlists", {name: playlistname})
-  return ("yeah")
-  // return (playlistname)
+  // var response
+  let id
+  // do {
+    response = http.oauthGetUrl("https://api.spotify.com/v1/me/playlists?limit=50", {format: 'json'})
+    // console.log(response.items)
+    // console.log(response.items.map((item) => {if (item.name == playlistname) return item.id }))
+    id = response.items.map((item) => {if (item.name == playlistname) return item.id }).slice(0,1).toString()
+    // console.log(response.items)
+    // console.log("this is a name " + name)
+    // count++
+    // if (name != playlistname)
+      // break
+  // } while (name != playlistname)
+  // return ("yeah")
+    response = http.oauthPostUrl("https://api.spotify.com/v1/playlists/" + id + "/tracks?uris=" + uris)
+  return ("Playlist created")
 }
