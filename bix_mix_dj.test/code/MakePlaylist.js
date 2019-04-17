c2f33f1425505e85592d52ae9f191a3bb9df3a51
Dylan Sotomayor <dysotoma@e1z4r6p21.42.us.org>
2019-04-16 21:52:54 -0700

    moreee trainging rene 🇲🇽 💖

--- bix_mix_dj.test/code/MakePlaylist.js
+++ bix_mix_dj.test/code/MakePlaylist.js
@@ -2,24 +2,12 @@
  var console = require('console')
 module.exports.function = function makePlaylist (Tracks, playlistname) {
   var response = http.oauthPostUrl("https://api.spotify.com/v1/me/playlists", JSON.stringify({"name": playlistname, "description": "a test to remove or keep you decide", "public": true}))
-// playlistname = "Indie Chillout"
   console.log(playlistname)
   let uris = Tracks.tracks.map((track) => {return ("spotify:track:" + track.id)}).toString()
   console.log(uris)
-  // var response
   let id
-  // do {
     response = http.oauthGetUrl("https://api.spotify.com/v1/me/playlists?limit=50", {format: 'json'})
-    // console.log(response.items)
-    // console.log(response.items.map((item) => {if (item.name == playlistname) return item.id }))
     id = response.items.map((item) => {if (item.name == playlistname) return item.id }).slice(0,1).toString()
-    // console.log(response.items)
-    // console.log("this is a name " + name)
-    // count++
-    // if (name != playlistname)
-      // break
-  // } while (name != playlistname)
-  // return ("yeah")
     response = http.oauthPostUrl("https://api.spotify.com/v1/playlists/" + id + "/tracks?uris=" + uris)
   return ("Playlist created")
 }
--- bix_mix_dj.test/code/Pause.js
+++ bix_mix_dj.test/code/Pause.js
@@ -1,24 +1,9 @@
  var http = require('http')
  var console = require('console')
 module.exports.function = function pause () {
- // var response = http.oauthGetUrl("https://api.spotify.com/v1/me/player/devices", {format: "json"})
-    // console.log(response)
-//  console.log(Object.keys(response));
-//  console.log(Object.value(response));
- 
-// console.log(response.devices.map((device) => device.id).toString())
-  // console.log("https://api.spotify.com/v1/me/player/pause/pause?device_id=" + response.devices.map((device) => device.id).toString())
-  // var newdevice = device.map(device_properties)
-  // console.log(resonse.body)
-  // var devices = response["devices"];
-  //var IDs = response["devices"].map(function(device) {return device["id"]});
-  
-  //console.log("this is what i got: " + response["details"])
   var response = http.oauthPutUrl("https://api.spotify.com/v1/me/player/pause")
    console.log(response)
-  // http.oauthGetUrl("https://api.spotify.com/v1/search?q=Muse&type=track")
-  // http.oauthGetUrl(url, options)
-  if (response == 403)
-    return ("You need a premium spotify account for this feature")
+  // if (response == 403)
+  //   return ("You need a premium spotify account for this feature")
   return ("Playback paused")
 }
