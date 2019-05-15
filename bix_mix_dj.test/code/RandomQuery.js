f3212b71a2828c58520737b19e7e8751e9fcddba
Dylan Sotomayor <dylants365@gmail.com>
2019-05-15 13:42:23 -0700

    randomquery added

--- bix_mix_dj.test/code/RandomQuery.js
+++ bix_mix_dj.test/code/RandomQuery.js
@@ -5,93 +5,95 @@
 
 module.exports.function = function randomNameQuery (numberOfSongs, requestedTempo) {
 
-  // If this was C, str[index] = character. But this is not C
-  function setCharAt(str, index, character) {
-    if (index > str.length - 1) {
-      return (str)
-    }
-    return (str.substr(0, index) + character + str.substr(index + 1))
-  }
-
   // Returns a random number between 0 and max - 1
   function getRandomNumber(max) {
     return (Math.floor(Math.random() * Math.floor(max)))
   }
 
   // Returns a simple string used to query the Spotify API.
-  // The string returned will contain 1 to 2 wildcards to help simulate randomness
-  // in song selection.
-  // Examples of returned string: *a*, *ut, lo*, etc.
   function getRandomQuery() {
-    var consonants = "bcdfghjklmnpqrstvwxyz"
-    var vowels = "aeiou"
-    var text = ""
+    let consonants = "bcdfghjklmnpqrstvwxyz"
+    let vowels = "aeiou"
+    let queryString = ""
 
-    // The possiblity of a name containing two consonants next to each other (*tk)
-    // is vastly lower than a name containing one consanant next to one vowel (*ta).
-    text += consonants.charAt(getRandomNumber(consonants.length))
-    text += vowels.charAt(getRandomNumber(vowels.length))
-    text += consonants.charAt(getRandomNumber(consonants.length))
-  
-    // Spotifys API allows up to two wild cards in their name query.
-    // Below a number (0 - 1) is generated.
-    // While that number is less than 2 insert a wildcard randomly into the query.
-    //   The random index to be replaced by a wild card is between 0 and text.length (0 - 2).
-    // Maximum wildcards to be inserted is 2 the minimum is 1
-    for (var i = getRandomNumber(2); i < 2; i++) {
-      var randomIndex = getRandomNumber(text.length)
-      if (text.charAt(randomIndex - 1) !== '*' && text.charAt(randomIndex + 1) !== '*')
-        text = setCharAt(text, randomIndex, '*')
-    }
-    return (text)
+    queryString += consonants.charAt(getRandomNumber(consonants.length))
+    queryString += '*'
+    queryString += vowels.charAt(getRandomNumber(vowels.length))
+    console.log(queryString)
+    return (queryString)
   }
 
   function querySpotify() {
-    var queryName = getRandomQuery()
-    var queryOffset = getRandomNumber(950)
-    var query = "https://api.spotify.com/v1/search?q=" + queryName + "&type=track&market=US&limit=50&offset=" + queryOffset
-    var data = http.oauthGetUrl(query, {format: "json"})
+    let queryName = getRandomQuery()
+    let query = "https://api.spotify.com/v1/search?q=" + queryName + "&type=track&market=US&limit=50"
+    let data = http.oauthGetUrl(query, {format: "json"})
     return (data)
   }
-  
+
+  function getTrackTempos(trackDetails) {
+    let tempos = []
+    for (let i = 0; i < 50; i++) {
+      if (trackDetails.audio_features[i] === null)
+        tempos[i] = 0
+      else
+        tempos[i] = trackDetails.audio_features[i].tempo
+    }
+    return (tempos)
+  }
+
+  function getDetailedTrackInfo(trackIDs) {
+    let query = "https://api.spotify.com/v1/audio-features?ids=" + trackIDs.join()
+    let trackDetails = http.oauthGetUrl(query, {format: "json"})
+    return (trackDetails)
+  }
+
   // Returns a list of track IDs as a single string delimited by commas
-  function getGeneralTrackInfo(data) {
-    var trackIDs = []
-    var trackNames = []
-    var images = []
+  function getTracks() {
+    let data = querySpotify()
+    let trackIDs = [], trackNames = [], images = []
     for (var i = 0; i < 50; i++) {
       trackIDs[i] = data.tracks.items[i].id
       trackNames[i] = data.tracks.items[i].name
       images[i] = data.tracks.items[i].album.images[0].url
     }
-    return [trackIDs, trackNames, images]
+    let details = getDetailedTrackInfo(trackIDs)
+    let tempos = getTrackTempos(details)
+    return {ids: trackIDs, names: trackNames, tempos: tempos, images: images}
   }
 
-  function getDetailedTrackInfo(trackIDs) {
-    var query = "https://api.spotify.com/v1/audio-features?ids=" + trackIDs
-    var trackDetails = http.oauthGetUrl(query, {format: "json"})
-    return (trackDetails)
-  }
-  
-  function getTrackTempos(trackDetails) {
-    var tempos = []
-    for (var i = 0; i < numberOfSongs; i++) {
-      tempos[i] = trackDetails.audio_features[i].tempo
+  function doTheThing() {
+    let ids = [], names = [], details = [], tempos = [], images = []
+    let tracks = null
+    let tooMany = 0, relax = 0, i = 0
+    while (i < numberOfSongs && tooMany < 100) {
+      tracks = getTracks()
+      for (let j = 0; j < 50 && i < numberOfSongs; j++) {
+        if (Math.floor(tracks.tempos[j]) >= requestedTempo - relax && Math.floor(tracks.tempos[j] <= requestedTempo + relax)) {
+          if (names.indexOf(tracks.names[j]) < 0) {
+            ids[i] = tracks.ids[j]
+            names[i] = tracks.names[j]
+            tempos[i] = tracks.tempos[j]
+            images[i] = tracks.images[j]
+            i += 1
+          }
+        }
+      }
+      tooMany += 1
+      if (tooMany % 5 === 0)
+        relax += tooMany % 4
     }
-    return (tempos)
+    return {ids: ids, names: names, tempos: tempos, images: images}
   }
 
-  var data = querySpotify()
-  var trackNames, trackIDs, images
-  [trackIDs, trackNames, images] = getGeneralTrackInfo(data)
-  var trackDetails = getDetailedTrackInfo(trackIDs.join())
-  var tempos = getTrackTempos(trackDetails)
+  info = doTheThing()
+  console.log(info.tempos)
+  console.log(info.names)
+  console.log(info.ids)
   return {
-    trackIDs: trackIDs,
-    tempos: tempos,
-    trackNames: trackNames,
-    trackAlbumImage: images,
+    trackIDs: info.ids,
+    tempos: info.tempos,
+    trackNames: info.names,
+    trackAlbumImage: info.images,
     trackTiming: "0"
   }
 }
-
