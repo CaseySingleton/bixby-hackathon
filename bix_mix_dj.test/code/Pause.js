66426401a9c1fd4538cf6b385b7912a35c456a52
Dylan Sotomayor <dylants365@gmail.com>
2019-05-15 13:42:17 -0700

    working pause but it needs a premium user

--- bix_mix_dj.test/code/Pause.js
+++ bix_mix_dj.test/code/Pause.js
@@ -1,8 +1,22 @@
  var secret = require('secret')
  var config = require('config')
  var http = require('http')
+ var console = require('console')
 module.exports.function = function auth () {
-  http.oauthGetUrl("https://api.spotify.com/v1/search?q=Muse&type=track")
+ var response = JSON.parse(http.oauthGetUrl("https://api.spotify.com/v1/me/player/devices"))
+    console.log(response)
+//  console.log(Object.keys(response));
+//  console.log(Object.value(response));
+ 
+console.log(response.devices.map((device) => device.id).toString())
+  // console.log("https://api.spotify.com/v1/me/player/pause/pause?device_id=" + response.devices.map((device) => device.id).toString())
+  // var newdevice = device.map(device_properties)
+  // console.log(resonse.body)
+  // var devices = response["devices"];
+  //var IDs = response["devices"].map(function(device) {return device["id"]});
+  
+  //console.log("this is what i got: " + response["details"])
+  var search = http.oauthPutUrl("https://api.spotify.com/v1/me/player/pause?device_id=" + response.devices.map((device) => device.id).toString())
+  // http.oauthGetUrl("https://api.spotify.com/v1/search?q=Muse&type=track")
   // http.oauthGetUrl(url, options)
-  return {}
 }
